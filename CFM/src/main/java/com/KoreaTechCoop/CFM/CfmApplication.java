2fd2d2c19d850019c7f3c1e91ca25c33f532eb28
정성욱 <rkfka7849@gmail.com>
2023-07-26 10:39:59 +0900

    프로젝트 개시

---
+++ CFM/src/main/java/com/KoreaTechCoop/CFM/CfmApplication.java
@@ -0,0 +1,13 @@
+package com.KoreaTechCoop.CFM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfmApplication.class, args);
+	}
+
+}
